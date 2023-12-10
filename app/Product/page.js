0652199297b319d600381e2696a128439ba94b28
mandira-05 <mandira.sn.gowda@gmail.com>
2023-12-10 11:26:13 +0530

    sheriyan-nextjs-first-video

---
+++ app/Product/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '@/Components/Header'
+
+const product = () => {
+ 
+    return (
+        <>
+           
+            Product
+        </>
+    )
+}
+
+export default product
